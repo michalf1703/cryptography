622489ca36e267d103026c6f7d738d6f6b51d192
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-04-27 11:07:48 +0200

    add gui model

--- ElGamal/src/main/java/com/example/elgamal/HelloController.java
+++ ElGamal/src/main/java/com/example/elgamal/HelloController.java
@@ -1,14 +1,101 @@
 package com.example.elgamal;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+
+    @FXML
+    private Button bntSzyfrowanie;
+
+    @FXML
+    private Button btnWczytajSzyfr;
+
+    @FXML
+    private Button btn_zapisz_tekst;
+
+    @FXML
+    private Button button_odszyfruj_plik_binarny;
+
+    @FXML
+    private Button button_wczytaj_plik_binarny;
+
+    @FXML
+    private Button button_wczytaj_tekst;
+
+    @FXML
+    private Button deszyfruj;
+
+    @FXML
+    private TextField szyfruj_plik;
+
+    @FXML
+    private TextField textField1;
+
+    @FXML
+    private TextField textField2;
+
+    @FXML
+    private TextField textField3;
+
+    @FXML
+    private TextField textField4;
+
+    @FXML
+    private TextField wczytaj_tekst;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button zapisz_szyfr;
+
+    @FXML
+    void btnDeszyfruj(ActionEvent event) {
+
     }
+
+    @FXML
+    void btnGenerujKlucz(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOdszyfrujPlikBinarny(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlikBinarny(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSzyfrowanie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnWczytajPlikTekst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnWczytajSzyfrPlik(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnZapiszPlikTekst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnZapiszSzyfr(ActionEvent event) {
+
+    }
+
 }
